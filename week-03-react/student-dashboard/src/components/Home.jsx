0f66168a64c70564a2e694ea5b0430c6b3807463
Abiola Fahad Ogunnaike <realabiolafahad@gmail.com>
2025-11-21 21:58:02 +0100

    Initialize student dashboard project with React, Vite, and Tailwind CSS
    - Created package.json with dependencies and scripts for development and build.
    - Implemented main App component with Header.
    - Created empty Home component for future implementation.
    - Added empty StudentCard and StudentForm components for student management.
    - Imported Tailwind CSS in index.css for styling.
    - Set up main entry point (main.jsx) for rendering the App component.
    - Configured Vite with React and Tailwind CSS plugins in vite.config.js.

---
+++ week-03-react/student-dashboard/src/components/Home.jsx
@@ -0,0 +1,12 @@
+import { useState, useEffect } from "react";
+import StudentCard from "./StudentCard"; // Check back later to see if file path caused any issue. "../components/StudentCard"
+import StudentForm from "./StudentForm";
+
+export default function Home(){
+
+
+
+
+
+    
+}
