e4adc2476fd5f8d9abe4b837580f07516c05c0b6
Abiola Fahad Ogunnaike <realabiolafahad@gmail.com>
2025-12-04 21:54:12 +0100

    Refactor Home component imports to include missing 'use' and API functions

--- week-03-react/student-dashboard/src/components/Home.jsx
+++ week-03-react/student-dashboard/src/components/Home.jsx
@@ -1,10 +1,13 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, use } from "react";
 import StudentCard from "./StudentCard"; // Check back later to see if file path caused any issue. "../components/StudentCard"
 import StudentForm from "./StudentForm";
+import { fetchStudent, createStudent, updateStudent, deleteStudent } from "../lib/api"
 
 export default function Home(){
-
-
+    const [ student, setStudent ] = useState([]);
+    const [ loading, setLoading ] = useState(false);
+    const [ error, setError ] = useState("");
+    
 
 
 
