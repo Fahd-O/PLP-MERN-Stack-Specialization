8f1cfa96d19f1e20e002d660171fc829b0ecebb4
Abiola Fahad Ogunnaike <realabiolafahad@gmail.com>
2025-12-05 23:01:00 +0100

    Fix fetchStudents function call and update handleAdd to correctly add new student

--- week-03-react/student-dashboard/src/components/Home.jsx
+++ week-03-react/student-dashboard/src/components/Home.jsx
@@ -1,14 +1,32 @@
 import { useState, useEffect, use } from "react";
 import StudentCard from "./StudentCard"; // Check back later to see if file path caused any issue. "../components/StudentCard"
 import StudentForm from "./StudentForm";
-import { fetchStudent, createStudent, updateStudent, deleteStudent } from "../lib/api"
+import { fetchStudents, createStudent, updateStudent, deleteStudent } from "../lib/api"
 
 export default function Home(){
     const [ student, setStudent ] = useState([]);
     const [ loading, setLoading ] = useState(false);
     const [ error, setError ] = useState("");
     
+    useEffect( () => {
+        (async() => {
+            try {
+                setLoading(true);
+                const data = await fetchStudents;
+                setStudent(data);
+            } catch (e) {
+                setError(e.message);                
+            }
+            finally {
+                setLoading(false);
+            }
+        })();
+    }, {});
 
+    async function handleAdd(student) {
+        const create = await createStudent(student);
+        setStudent(prev=>[created, ...prev]);
+    }
 
 
     
