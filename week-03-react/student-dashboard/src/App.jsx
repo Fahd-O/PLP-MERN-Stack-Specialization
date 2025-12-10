701684181ebbadedcc540425719b9988fb0cc5ce
Abiola Fahad Ogunnaike <realabiolafahad@gmail.com>
2025-12-10 23:54:07 +0100

    Fix title casing in index.html and uncomment Home component in App.jsx

--- week-03-react/student-dashboard/src/App.jsx
+++ week-03-react/student-dashboard/src/App.jsx
@@ -1,11 +1,11 @@
 import Header from "./components/Header";
-// import Home from "./components/Home.jsx";
+import Home from "./components/Home.jsx";
 
 export default function App(){
   return(
     <>
       <Header />
-      {/* <Home /> */}
+      <Home />
     </>
-  )
+  );
 }
