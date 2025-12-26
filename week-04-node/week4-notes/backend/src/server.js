8e989bb983e7d3f83977054f475d932371795c08
Abiola Fahad Ogunnaike <realabiolafahad@gmail.com>
2025-12-26 21:50:37 +0100

    Build server for week 4

---
+++ week-04-node/week4-notes/backend/src/server.js
@@ -0,0 +1,27 @@
+const expressContainer = require("express");
+const corsContainer = require("cors");
+const dotenvContainer = require("dotenv");
+const { connectDB } = require("./config/db");
+const noteRouter = require("./routes/notes");
+
+
+dotenvContainer.config();
+const app = expressContainer();
+
+connectDB();
+
+
+app.use(corsContainer({
+    origin: process.env.ALLOWED_ORIGIN,
+    methods: ["GET", "POST", "PUT", "DELETE"]
+}));
+
+app.use(expressContainer.json());
+
+app.get("/", (req, res) => res.send("API is up and running... Don't fully understand what API means though"));
+app.use("/api/notes", noteRouter);
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, ()=> console.log(`API is skating on http://localhost:${PORT}`));
