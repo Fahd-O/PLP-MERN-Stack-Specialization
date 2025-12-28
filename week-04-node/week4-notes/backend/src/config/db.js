c563bf1c150b5eca6b9aaba4a120cc2a37b49957
Abiola Fahad Ogunnaike <realabiolafahad@gmail.com>
2025-12-28 22:25:23 +0100

    feat: enhance log messages in db.js and server.js for better clarity

--- week-04-node/week4-notes/backend/src/config/db.js
+++ week-04-node/week4-notes/backend/src/config/db.js
@@ -3,7 +3,7 @@ const mongooseContainer = require('mongoose');
 async function connectDB() {
     try {
         await mongooseContainer.connect(process.env.MONGODB_URI);
-        console.log('Oh, the connection actually happened! Connected to Localhost MongoDB is a success! Making progress in Week 4... onward!!!');
+        console.log('Oh, the connection actually happened! Connected to Localhost MongoDB is a success! Making progress in Week 4... onward!!! This msg is from the db.js file');
     } catch (error) {
          console.error('MongoDB error: ', error.message);
          process.exit(1);
--- week-04-node/week4-notes/backend/src/server.js
+++ week-04-node/week4-notes/backend/src/server.js
@@ -18,10 +18,10 @@ app.use(corsContainer({
 
 app.use(expressContainer.json());
 
-app.get("/", (req, res) => res.send("API is up and running... Don't fully understand what API means though"));
+app.get("/", (req, res) => res.send("API is up and running... Don't fully understand what API means though. This msg is from the server.js file"));
 app.use("/api/notes", noteRouter);
 
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, ()=> console.log(`API is skating on http://localhost:${PORT}`));
+app.listen(PORT, ()=> console.log(`API is skating on http://localhost:${PORT}. This msg is from the server.js file`));
