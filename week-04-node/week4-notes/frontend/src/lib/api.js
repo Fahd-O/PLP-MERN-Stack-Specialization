71da428b590f5a0d3a109d231a0cb90368e586f0
Abiola Fahad Ogunnaike <realabiolafahad@gmail.com>
2026-01-01 18:22:35 +0100

    feat: add Card, Input, Textarea components and NotesAPI for enhanced UI and API interactions

---
+++ week-04-node/week4-notes/frontend/src/lib/api.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+import { Update } from "vite";
+
+const API = import.meta.env.VITE_API_URL;
+const client = axios.create(
+    {
+        baseURL: API,
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }
+);
+
+
+// Notes Endpoints
+export const NotesAPI = {
+    list: async (userId) => {
+        const res = await client.get(`/api/notes`, { params: userId ? { userId } : {} });
+        return res.data;
+    },
+
+    create: async (payload) => {
+        const res = await client.post(`/api/notes`, payload);
+        return res.data;
+    },
+
+    Update: async (id, payload) => {
+        const res = await client.put(`/api/notes${id}`, payload);
+        return res.data;
+    },
+
+    remove: async (id) => {
+        const res = await client.delete(`/api/notes${id}`);
+        return res.data;
+    }
+}
